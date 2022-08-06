c9620de481c000de90411152b5625046640a6742
Gabriel <gabriel.boeira@softplan.com.br>
2022-08-06 15:47:36 -0300

    First commit

---
+++ api.js
@@ -0,0 +1,79 @@
+const axios = require('axios');
+const querystring = require('querystring');
+const crypto = require('crypto');
+const { Console } = require('console');
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.SECRET_KEY;
+const apiUrl = process.env.API_URL;
+
+
+async function privateCall(path, data = {}, method = 'GET') {
+    if (!apiKey || !apiSecret)
+        throw new Error('Preencha corretamente sua API KEY e SECRET KEY');
+
+    const timestamp = Date.now();
+    const recvWindow = 60000;//máximo permitido, default 5000
+
+    const signature = crypto
+        .createHmac('sha256', apiSecret)
+        .update(`${querystring.stringify({ ...data, timestamp, recvWindow })}`)
+        .digest('hex');
+
+    const newData = { ...data, timestamp, recvWindow, signature };
+    const qs = `?${querystring.stringify(newData)}`;
+
+    try {
+        const result = await axios({
+            method,
+            url: `${apiUrl}${path}${qs}`,
+            headers: { 'X-MBX-APIKEY': apiKey }
+        });
+        return result.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function accountInfo() {
+    return privateCall('/v3/account');
+}
+
+async function newOrder(symbol, quantity, price, side = 'BUY', type = 'MARKET') {
+    const data = { symbol, side, type, quantity }
+
+    if (type !== 'MARKET') {
+        if (price) data.price = parseFloat(price).toFixed(5);
+    }
+
+    if (type === 'LIMIT') data.timeInForce = 'GTC';
+
+    return privateCall('/v3/order', data, 'POST');
+}
+
+
+async function publicCall(path, data, method = 'GET') {
+    try {
+        const qs = data ? `?${querystring.stringify(data)}` : '';
+        const result = await axios({
+            method,
+            url: `${apiUrl}${path}${qs}`
+        });
+        return result.data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function time() {
+    return publicCall('/v3/time');
+}
+
+async function depth(symbol = 'BTCBRL', limit = 5) {
+    return publicCall('/v3/depth', { symbol, limit });
+}
+
+async function exchangeInfo() {
+    return publicCall('/v3/exchangeInfo');
+}
+
+module.exports = { time, depth, exchangeInfo, accountInfo, newOrder }
---
+++ app.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const api = require('./api');
+const app = express();
+const path = require('path');
+const profitability = process.env.PROFITABILITY;
+const symbol = process.env.SYMBOL;
+const interval = process.env.CRAWLER_INTERVAL;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/data', async (req, res) => {
+
+    const data = {};
+
+    const mercado = await api.depth(symbol);    
+    data.buy = mercado.bids.length ? mercado.bids[0][0] : 0;
+    data.sell = mercado.asks.length ? mercado.asks[0][0] : 0;
+
+    const carteira = await api.accountInfo();
+    const coins = carteira.balances.filter(b => symbol.indexOf(b.asset) !== -1);
+    data.coins = coins;
+
+    const sellPrice = parseFloat(data.sell);
+
+    //-----------------------//
+    //Lógica do bot - INÍCIO //
+    //-----------------------//
+
+    ////--POSICIONANDO COMPRA
+    console.log('Verificando se tenho grana...');
+    //--COMPRAR  
+    if (sellPrice && sellPrice < 400.40) {
+        
+        console.log('Consultar carteira - Bom de comprar');
+        const carteira = await api.accountInfo();
+        const coins = carteira.balances;
+        //--const coins = carteira.balances.filter(b => b.asset === 'BUSD' || b.asset === 'BNBBUSD');
+        // console.log('----------------Resultado da carteira----------------');
+        // console.log(coins);
+        // console.log('-----------------------------------------------------');
+        
+        
+        ////--POSICIONANDO COMPRA
+        console.log('Verificando se tenho grana...');
+        // --const walletCoin = parseFloat(coins.find(c => c.asset.endsWith('USD')).free); // Em produção poderá dar diferença devido a nomes das moedas.
+        const walletCoin = parseFloat(coins.find(c => c.asset === 'BUSD').free).toFixed(5);
+        // const qty = parseFloat((walletCoin / sellPrice) - 0.00001).toFixed(5); //Cálculo para dividir quantity em fação.
+        const qty = 1 //TESTE
+        console.log(`Qty: ${qty}`);
+        console.log(`Total Coin:  ${walletCoin}`);
+
+        if (qty > 0) {
+            //--Ordem de compra: console.log(await api.newOrder(symbol, 1)) //situação geral da operação
+            const buyOrder = await api.newOrder(symbol, qty);
+            data.buyOrder = buyOrder;   
+            console.log(`orderId: ${buyOrder.orderId}`);
+            console.log(`status: ${buyOrder.status}`);
+
+
+            //--POSICIONANDO VENDA
+            console.log('Posicionando venda futura!!!');
+            const price = parseFloat(sellPrice * profitability).toFixed(5);
+
+            console.log(`Vendendo por ${price} (${profitability})`);
+            const sellOrder = await api.newOrder(symbol, qty, price, 'SELL', 'MARKET');
+            data.sellOrder = sellOrder;
+            console.log(`orderId: ${sellOrder.orderId}`);
+            console.log(`status: ${sellOrder.status}`);
+        }
+
+        console.log('----------------Resultado da carteira----------------');
+        console.log(coins);
+        console.log('-----------------------------------------------------');
+
+        //--VENDER
+    } else if (buy && buy > 1000) {
+        console.log('Bom para vender');
+
+        //--AGUARDANDO MERCADO
+    // } else {
+        console.log('Aguardando mercado');
+    }
+
+    //-----------------------//
+    //Lógica do bot - FIM //
+    //-----------------------//
+
+    res.json(data);
+
+})
+
+app.use('/', (req, res) => {
+    console.log('Entrou');
+    res.render('index', {
+        symbol: symbol,
+        profitability: profitability,
+        lastUpdate: new Date(),
+        interval: parseInt(interval)
+    });
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log('App rodando');
+})
